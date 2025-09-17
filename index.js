236d376321e6db1a8954fa20305128f4e0d2b399
Your Name <you@example.com>
2025-09-17 12:11:02 +0530

    commited

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors({
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 // ------------------ User Schema ------------------
 const userSchema = new mongoose.Schema({
