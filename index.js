1cedb6a823661b800f73a353f4464b1217c63f71
Your Name <you@example.com>
2025-09-17 11:28:38 +0530

    commited

--- index.js
+++ index.js
@@ -142,6 +142,18 @@ app.get('/api/users', async (req, res) => {
   }
 });
 
+// Delete user by ID
+app.delete('/api/users/:id', async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) return res.status(404).json({ message: 'User not found' });
+    res.status(200).json({ message: 'User deleted successfully', user: deletedUser });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
 
 
 // ------------------ Job Routes ------------------
